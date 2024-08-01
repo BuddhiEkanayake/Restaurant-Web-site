fc18862638a726fac9df869b40e9446c4d68f33f
BuddhiEkanayake <buddhisri12@gmail.com>
2024-08-01 14:44:18 +0530

    Assets and components initialized

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import About from './Components/About';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Testimonial from './Components/Testimonial';
+import Work from './Components/Work';
+
+function App() {
+  return (
+    <div className="App">
+     <Home/>
+     <About/>
+     <Work/>
+     <Testimonial/>
+     <Footer/>
+     
+    </div>
+  );
+}
+
+export default App;
